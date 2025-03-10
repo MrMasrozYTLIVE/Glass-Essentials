a0390aebb64fbb32eae7f56ed327323e7b94b029
calmilamsy <bumbill00@gmail.com>
2025-03-10 15:32:29 +0000

    Seething

--- src/main/java/net/glasslauncher/glassbrigadier/mixin/brigadierjank/FixRecursiveCheck.java
+++ src/main/java/net/glasslauncher/glassbrigadier/mixin/brigadierjank/FixRecursiveCheck.java
@@ -11,8 +11,19 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public abstract class FixRecursiveCheck<S> {
     @Shadow protected abstract String getSmartUsage(CommandNode<S> node, S source, boolean optional, boolean deep);
 
-    @Redirect(method = "getSmartUsage(Lcom/mojang/brigadier/tree/CommandNode;Ljava/lang/Object;ZZ)Ljava/lang/String;", at = @At(value = "INVOKE", target = "Lcom/mojang/brigadier/CommandDispatcher;getSmartUsage(Lcom/mojang/brigadier/tree/CommandNode;Ljava/lang/Object;ZZ)Ljava/lang/String;", ordinal = 0))
-    String test(CommandDispatcher<S> instance, CommandNode<S> node, S source, boolean child, boolean deep) {
-        return getSmartUsage(node, source, child, false);
+    // Fixes a **REALLY** stupid deep check which is set to childOptional for SOME reason?
+    // This forces it to always be false so single nodes after an optional node will actually be fucking looked at. Don't ask why deep has reversed logic, I don't fucking know.
+    // You wasted an hour of my time.
+    @Redirect(
+            method = "getSmartUsage(Lcom/mojang/brigadier/tree/CommandNode;Ljava/lang/Object;ZZ)Ljava/lang/String;",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/mojang/brigadier/CommandDispatcher;getSmartUsage(Lcom/mojang/brigadier/tree/CommandNode;Ljava/lang/Object;ZZ)Ljava/lang/String;",
+                    ordinal = 0
+            ),
+            require = 0
+    )
+    String test(CommandDispatcher<S> instance, CommandNode<S> source, S child, boolean optional, boolean deep) {
+        return getSmartUsage(source, child, optional, false);
     }
 }
