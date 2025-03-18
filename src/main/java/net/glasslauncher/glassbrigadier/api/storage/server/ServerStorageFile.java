9d9c0c8a5fe38e740dfbb1adcb5115addb7ba2f4
calmilamsy <bumbill00@gmail.com>
2025-03-18 16:25:23 +0000

    Haha oops

--- src/main/java/net/glasslauncher/glassbrigadier/api/storage/server/ServerStorageFile.java
+++ src/main/java/net/glasslauncher/glassbrigadier/api/storage/server/ServerStorageFile.java
@@ -6,6 +6,8 @@ import net.glasslauncher.glassbrigadier.GlassBrigadier;
 import net.glasslauncher.glassbrigadier.api.storage.StorageFile;
 import net.glasslauncher.glassbrigadier.api.storage.StorageUtils;
 import net.minecraft.entity.player.PlayerEntity;
+import net.modificationstation.stationapi.api.util.Identifier;
+import net.modificationstation.stationapi.api.util.Namespace;
 import org.jetbrains.annotations.NotNull;
 import org.simpleyaml.configuration.Configuration;
 import org.simpleyaml.configuration.MemoryConfiguration;
@@ -17,9 +19,9 @@ import java.util.function.Function;
 public class ServerStorageFile extends StorageFile {
     public static final Configuration SERVER_DATA_DEFAULTS = new MemoryConfiguration();
 
-    private static final @NotNull Cache<@NotNull String, @NotNull ServerStorageFile> CACHE = Caffeine.newBuilder().softValues().build();
-    private static final @NotNull Function<@NotNull String, @NotNull ServerStorageFile> PLAYER_STORAGE_FILE_FACTORY = (playerName) -> {
-        ServerStorageFile file = new ServerStorageFile(StorageUtils.getWorldDir());
+    private static final @NotNull Cache<@NotNull Identifier, @NotNull ServerStorageFile> CACHE = Caffeine.newBuilder().softValues().build();
+    private static final @NotNull Function<@NotNull Identifier, @NotNull ServerStorageFile> PLAYER_STORAGE_FILE_FACTORY = (identifier) -> {
+        ServerStorageFile file = new ServerStorageFile(StorageUtils.getModStorageFile(identifier));
         try {
             file.load();
         } catch (IOException e) {
@@ -32,12 +34,12 @@ public class ServerStorageFile extends StorageFile {
         CACHE.invalidateAll();
     }
 
-    public static StorageFile of(PlayerEntity player) {
-        return of(player.name);
+    public static StorageFile of(Namespace namespace) {
+        return of(namespace.id("storage"));
     }
 
-    public static StorageFile of(String playerName) {
-        return CACHE.get(playerName, PLAYER_STORAGE_FILE_FACTORY);
+    public static StorageFile of(Identifier identifier) {
+        return CACHE.get(identifier, PLAYER_STORAGE_FILE_FACTORY);
     }
 
     protected ServerStorageFile(File file) {
